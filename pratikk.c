ba56c5fccd7e5106394443fc8a95868921954fd6
pratikskamble <pratikkamble24008@gmail.com>
2026-07-25 21:42:01 +0530

    c program(circle area)

---
+++ pratikk.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main(){
+    int r;
+    printf("enter r");
+    scanf("%d", &r);
+// area of the circle
+    float pi;
+    printf("enter pi");
+    scanf("%f", pi);
+
+    print("area of the circle = %f", r * r * pi);
+
+    return 0;
+    
+
+}
