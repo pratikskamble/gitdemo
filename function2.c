d45f1ae207b0751e9c30e49831ac53c797b20ecc
gauri sadafule <gauri.sada123@gmail.com>
2026-07-26 13:38:36 +0530

    temperature program

---
+++ function2.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main (){
+    // formula of celcius = 9/5*(f-32)
+    
+    float f;
+    printf("enter f");
+    scanf("%f", &f);
+
+    printf("c = %f", ((9/5) * (f-32)));
+    return 0;
+
+    
+}
