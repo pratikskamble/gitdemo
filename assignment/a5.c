488a3c84a56feb145e7d0e35c439e27eecef5091
gauri sadafule <gauri.sada123@gmail.com>
2026-07-27 18:45:11 +0530

    commit

---
+++ assignment/a5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    //printing arithmetic mean (a + b)/2
+
+    int a;
+    printf("enter a"); 
+    scanf("%d", &a);
+
+   int b;
+ printf("enter b"); 
+    scanf("%d", &b);
+
+    printf("AM = %f", ((a+b) / 2));
+    return 0;
+
+
+}
